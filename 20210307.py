1b743eaa548d01ffc9e69dc17ce4e97e07b69b10
Lan2008-StudioPro <10955015@gs.hs.ntnu.edu.tw>
2021-03-07 11:30:48 +0800

    int, str, print, max, input

---
+++ 20210307.py
@@ -0,0 +1,12 @@
+print('Mighty computer:',3+67/5+3)
+af=max('1','11','0.01')
+print(af)
+#Weight=input('請輸入體重')#輸入體重
+#print(Weight)
+#Height=input('請輸入身高')#輸入身高
+#print(Height)
+
+e=int(input())#int用於文字轉換數字
+print(e*5)
+f=str(2)
+print(f*20)
