8ccfb12b75371ada933fe7ca5277addf303380c5
Lan2008-StudioPro <10955015@gs.hs.ntnu.edu.tw>
2021-04-29 06:07:00 +0800

    HW

--- Homework/20210425/t0_draw_house.py
+++ Homework/20210425/t0_draw_house.py
@@ -2,3 +2,42 @@
 請使用def畫出10個，不同顏色的屋頂，及位置的房子
 顏色用list用代入
 '''
+import random as r
+import turtle as t
+list_color = ['lightcoral', 'darkturquoise', 'springgreen',
+              'wheat', 'dodgerblue', 'crimson', 'fuchsia',
+              'chartreuse', 'mediumslateblue', 'tan']
+def draw_house(a: int):
+    house = 0
+    while house != a:
+        t.pu()
+        width = r.randint(-2, 10)
+        length = r.randint(8, 30)
+        x = r.randrange(-600, 600, 30)
+        y = r.randrange(-200, 200, 10)
+        t.color(list_color[r.randint(0, 9)])
+        t.goto(x, y)
+        t.seth(180)
+        t.ht()
+        t.speed(0)
+        t.pd()
+        t.begin_fill()
+        t.fd(50)
+        t.goto(x - 50 - 7, y - 24)
+        t.bk(64)
+        t.goto(x, y)
+        t.end_fill()  # End of roof-drawing.
+        t.pu()
+        t.goto(x - width, y - 24)
+        t.color(list_color[r.randint(0, 9)])
+        t.pd()
+        t.begin_fill()
+        t.goto(x - width, y - 24 - length)
+        t.goto(x - width - (25 - width) * 2, y - 24 - length)
+        t.goto(x - width - (25 - width) * 2, y - 24)
+        t.end_fill()  # End of base-drawing.
+        t.pu()
+        house += 1
+
+draw_house(100)  # Enter the amount of houses you want to draw with TURTLE.
+
--- Homework/20210425/t1_lcm.py
+++ Homework/20210425/t1_lcm.py
@@ -3,3 +3,38 @@
 並使用function def 求最小公倍數
 value = lcm(num1, num2)
 '''
+def lcm(m, n):
+    try:
+        m = int(m)
+        n = int(n)
+    except ValueError:
+        print('ValueError')
+    else:
+        list_lcm_m = []
+        list_lcm_n = []
+        cfactors = []
+        lcmnum = 1
+        while m > 1:
+            for a in range(2, m):
+                while m % a == 0:
+                    list_lcm_m.append(a)
+                    m /= a
+            else:
+                list_lcm_m.append(m)
+        cfactors.extend(list_lcm_m)
+        while n > 1:
+            for b in range(2, n):
+                while n % b == 0:
+                    list_lcm_n.append(b)
+                    n /= b
+            else:
+                list_lcm_n.append(n)
+        cfactors.extend(list_lcm_n)
+        for factorcheck in cfactors:
+            if list_lcm_m.count(factorcheck) > list_lcm_n.count(factorcheck):
+                lcmnum = factorcheck ** list_lcm_m.count(factorcheck)
+            else:
+                lcmnum = factorcheck ** list_lcm_n.count(factorcheck)
+        print(int(lcmnum))
+
+lcm(45, 72)
