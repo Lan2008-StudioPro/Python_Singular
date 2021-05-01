78f0aa89413ec14e046a2aec16415455d656515b
Lan2008-StudioPro <10955015@gs.hs.ntnu.edu.tw>
2021-05-02 07:19:25 +0800

    HW

--- Homework/20210425/t1_lcm.py
+++ Homework/20210425/t1_lcm.py
@@ -4,17 +4,22 @@
 value = lcm(num1, num2)
 '''
 def lcm(m, n):
+    import sys
     try:
-        m = int(m)
-        n = int(n)
+        int(m)
+        int(n)
     except ValueError:
         print('ValueError')
+    except TypeError:
+        print('TypeError')
     else:
+        m = int(m)
+        n = int(n)
         list_lcm_m = [m]
         list_lcm_n = [n]
         cfactors = []
         lcmnum = 1
-        while m > 1:
+        if m > 1 and type(m) == int:
             for x in range(2, m):
                 if m % x == 0:
                     list_lcm_m.remove(m)
@@ -22,9 +27,10 @@ def lcm(m, n):
                         while m % a == 0:
                             list_lcm_m.append(a)
                             m /= a
+        else:
+            sys.exit('\nValueError - input should be a natural number')
         cfactors.extend(list_lcm_m)
-        print(list_lcm_m)
-        while n > 1:
+        if n > 1 and type(n) == int:
             for y in range(2, n):
                 if n % y == 0:
                     list_lcm_n.remove(n)
@@ -32,16 +38,15 @@ def lcm(m, n):
                         while n % a == 0:
                             list_lcm_n.append(a)
                             n /= a
+        else:
+            sys.exit('\nValueError - input sholud be a natural number')
         cfactors.extend(list_lcm_n)
-        print(list_lcm_n)
         cfactors = set(cfactors)
-        print(cfactors)
         for fcheck in cfactors:
             if list_lcm_m.count(fcheck) > list_lcm_n.count(fcheck):
                 lcmnum *= fcheck ** list_lcm_m.count(fcheck)
             else:
                 lcmnum *= fcheck ** list_lcm_n.count(fcheck)
-            print(lcmnum)
         print(int(lcmnum))
 
-lcm(18, 8)
+lcm(input(), input())
